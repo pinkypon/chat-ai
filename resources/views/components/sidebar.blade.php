f84f880dfcfcd5686910d7d7d189b93266ebe62d
unknown <facethereality01@gmail.com>
2025-09-07 11:15:12 +0800

    fix the trash icon button on delete chat

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -60,8 +60,21 @@
                     :class="{ 'cursor-not-allowed': Alpine.store('chat').loading }"
                     @click="showModal = true;
                             deleteUrl = '{{ route('chat.delete', $conv->id) }}';">
-              <!-- trash icon -->
+
+                <!-- Trash icon -->
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    class="w-4 h-4 text-gray-500 hover:text-red-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 
+                          00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+
             </button>
+
           </div>
         @endforeach
       @else
@@ -81,7 +94,7 @@
               d="M5.121 17.804A13.937 13.937 0 0112 15c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0z" />
       </svg>
       <span class="truncate block max-w-[12rem]" title="{{ Auth::user()->email }}">
-        {{ Auth::user()->email }}adasdas
+        {{ Auth::user()->email }}
       </span>
     </button>
     <div x-show="open" x-cloak @click.outside="open = false"
